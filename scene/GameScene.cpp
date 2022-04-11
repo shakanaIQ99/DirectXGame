3419d608e7eee161acd49b92ad521de396320496
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2022-04-11 16:57:12 +0900

    02_01_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,8 +7,9 @@ using namespace DirectX;
 GameScene::GameScene() {}
 
 GameScene::~GameScene() 
-{
-	
+{ 
+	delete model_;
+
 }
 
 void GameScene::Initialize() {
@@ -17,12 +18,35 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	model_ = Model::Create();
 	
+	// x,y,z方向のスケーリングを特定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	//x,y,z軸周りの回転角を設定
+	worldTransform_.rotation_ = {XMConvertToRadians(45.0f), XMConvertToRadians(45.0f), 0.0f};
+	//
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
 }
 
 void GameScene::Update() 
-{
-	
+{ 
+	debugText_->SetPos(50, 50);
+	debugText_->Printf("scale:%f,%f,%f", worldTransform_.scale_.x, worldTransform_.scale_.y,worldTransform_.scale_.z);
+	debugText_->SetPos(50, 70);
+	debugText_->Printf(
+	  "rotation:%f,%f,%f", worldTransform_.rotation_.x, worldTransform_.rotation_.y,
+	  worldTransform_.rotation_.z);
+	debugText_->SetPos(50, 90);
+	debugText_->Printf(
+	  "translation:%f,%f,%f", worldTransform_.translation_.x, worldTransform_.translation_.y,
+	  worldTransform_.translation_.z);
+
 }
 
 void GameScene::Draw() {
@@ -51,6 +75,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
